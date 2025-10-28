4ce28821fffdddeaf388e6524236de7c3e89c60f
Bayzid004 <bayzid4440@gmail.com>
2025-10-28 18:12:25 +0600

    padding nav

--- src/components/navbarSection/CenterNavbar.tsx
+++ src/components/navbarSection/CenterNavbar.tsx
@@ -49,7 +49,7 @@ export default function CenterNavbar() {
   ];
 
   return (
-    <header className="bg-white border-b border-gray-200 py-2 px-4 flex justify-between items-center relative">
+    <header className="bg-white border-b border-gray-200 py-2 p-x-192 flex justify-between items-center relative">
       {/* Left Section */}
       <div className="flex items-center gap-2 sm:gap-4">
         <Link
