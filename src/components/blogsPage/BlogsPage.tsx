57e3c264cea4a83f7f8bc6b7a7980feebc53c020
Bayzid004 <bayzid4440@gmail.com>
2025-11-04 22:39:18 +0600

    blog page added

--- src/components/blogsPage/BlogsPage.tsx
+++ src/components/blogsPage/BlogsPage.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Banner from "./bannerSection/Banner";
 import AboutTorontoHandymanSection from "./aboutTorontoHandymanSection/AboutTorontoHandymanSection";
+import ReadyToWorkSection from "../homePage/readyToWorkSection/ReadyToWorkSection";
+import OurClientSaying from "../homePage/ourClientSaying/OurClientSaying";
+import OurServiceAreas from "../galleryPage/OurServiceAreas/OurServiceAreas";
+import Map from "../common/Map";
+import GetInTouch from "../homePage/getInTouch/GetInTouch";
 
 const BlogsPage: React.FC = () => {
   return (
@@ -12,7 +17,22 @@ const BlogsPage: React.FC = () => {
         <section>
           <AboutTorontoHandymanSection />
         </section>
+        <section>
+          <ReadyToWorkSection />
+        </section>
+        <section>
+          <OurClientSaying />
+        </section>
+        <section>
+          <OurServiceAreas />
+        </section>
+        <section>
+          <GetInTouch />
+        </section>
       </div>
+      <section>
+        <Map />
+      </section>
     </main>
   );
 };
